4654ff9efc7b0255d2e4dda08017f615fdc7a3ba
dhwanil-ditani <ditanidhwanil@gmail.com>
2021-06-15 17:01:06 +0530

    add api_key

--- converter/views.py
+++ converter/views.py
@@ -1,6 +1,15 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 
+key = "636d309899e0cc52dfc97276b08774cfa68e9e2d"
+
 def index(request):
-    return render(request, 'index.html')
+    parameters = {"api_key": key, "format": "json"}
+    url = "https://api.getgeoapi.com/v2/currency/list"
+    response = requests.get(url, parameters)
+    return HttpResponse(response)
+
+
