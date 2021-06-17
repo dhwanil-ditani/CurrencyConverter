0377f3ebfba77e352bfae3567009dbb76f291560
dhwanil-ditani <ditanidhwanil@gmail.com>
2021-06-17 16:25:53 +0530

    graph

--- converter/views.py
+++ converter/views.py
@@ -4,12 +4,18 @@ import requests
 
 # Create your views here.
 
-key = "636d309899e0cc52dfc97276b08774cfa68e9e2d"
+key1 = "636d309899e0cc52dfc97276b08774cfa68e9e2d"
+url1 = "https://api.getgeoapi.com/v2/currency/list"
+key2 = "226f17e7fe20f207c512"
+url2 = "https://free.currconv.com/api/v7/convert"
 
 def index(request):
-    parameters = {"api_key": key, "format": "json"}
-    url = "https://api.getgeoapi.com/v2/currency/list"
-    response = requests.get(url, parameters)
-    return HttpResponse(response)
+    return render(request, "index.html")
+
+def graph(request):
+    parameters = {"q": "USD_INR", "compact": "ultra", "date": "2021-01-01", "endDate": "2021-01-08", "apiKey": key2}
+    response = requests.get(url2, parameters)
+    context = {"data": response.json()}
+    return render(request, "graph.html", context)
 
 
